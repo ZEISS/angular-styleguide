62da2633b49cca9ce848f9dc9224536d8c4aff55
Stefan Bley <stefan.bley@saxsys.de>
2020-03-09 13:54:13 +0100

    Remove unused imports

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '@models/product';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { ProductService } from '../../services/product.service';
-import { switchMap } from 'rxjs/operators';
 import { NavigationActions } from '@store/navigation.actions';
 import { select } from '@angular-redux/store';
 import { getCurrentProductDetail } from '@app/product/store/selectors';
