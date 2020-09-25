47d2fa9a49cba01037d0aeeb07c470d070bb6987
Stefan Bley <stefan.bley@zeiss.com>
2020-09-25 09:54:29 +0200

    Remove unnecessary recommendation actions unit test

--- src/app/catalog/recommendation/store/recommendation.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromRecommendation from './recommendation.actions';
-
-describe('loadRecommendations', () => {
-  it('should return an action', () => {
-    expect(fromRecommendation.loadRecommendations().type).toBe('[Recommendation] Load Recommendations');
-  });
-});
