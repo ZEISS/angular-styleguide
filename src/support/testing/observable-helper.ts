a0e026e8d017ce701c560f3196e482db5aab06ba
stefan.heinze <stefan.heinze@zeiss.com>
2022-04-01 12:51:14 +0200

    add documentation for testObservable function

--- src/support/testing/observable-helper.ts
+++ src/support/testing/observable-helper.ts
@@ -1,5 +1,11 @@
 import { RunHelpers, TestScheduler } from 'rxjs/testing';
 
+/**
+ * Helper method for creating a test scheduler in order to use marble testing.
+ *
+ * @param testCallback for checking the expected observable results
+ * @param invertAssertion flag for checking objects to be equals or not to be equals
+ */
 export function testObservable(
   testCallback: (runHelpers: RunHelpers, scheduler: TestScheduler) => void,
   invertAssertion?: boolean
