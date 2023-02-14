d5a2df6822178b2ba164a73bf10478b9a418f443
Marius Unger <marius.unger@zeiss.com>
2023-02-14 14:35:16 +0100

    update angular@15 & fontawesome@0.12.1 (@angular/cli; @angular/core; @angular-eslint/schematics; @ngrx/store)

--- src/test.ts
+++ src/test.ts
@@ -12,22 +12,7 @@ import {
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false },
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.(spec|matchers)\.ts$/);
-// And load the modules.
-context.keys().map(context);
