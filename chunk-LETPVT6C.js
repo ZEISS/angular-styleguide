e86d6d137e53435b52703c4b7799c5f2d99c654c
LBraeschke <LBraeschke@users.noreply.github.com>
2025-11-07 13:32:30 +0000

    deploy: 10e7c921f16b7596a502966e6a4d9174811bf432

---
+++ chunk-LETPVT6C.js
@@ -0,0 +1,6 @@
+import{Bb as r,Cb as d,Db as l,Gb as c,Pb as p,Qa as o,Qb as s,Ra as m,Sb as g,_a as a}from"./chunk-EQV3HX4A.js";var h=(()=>{let e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=a({type:e,selectors:[["app-recommendation"]],inputs:{recommendation:"recommendation"},decls:3,vars:3,consts:[["alt","recommendation image",1,"recommendation-image",3,"src"],[1,"recommendation-title"]],template:function(t,n){t&1&&(l(0,"img",0),r(1,"p",1),p(2),d()),t&2&&(c("src",g(n.recommendation==null?null:n.recommendation.image),o),m(2),s(n.recommendation==null?null:n.recommendation.title))},styles:[`.recommendation-title[_ngcontent-%COMP%]{text-align:right;padding-right:20px;padding-left:20px;overflow:hidden;white-space:nowrap;text-overflow:ellipsis;font-size:20px}.recommendation-image[_ngcontent-%COMP%]{width:100%}
+
+
+
+
+`],changeDetection:0});let i=e;return i})();export{h as RecommendationComponent};
